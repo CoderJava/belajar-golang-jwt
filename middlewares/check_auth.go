4b427663d9512cb44080cb4bfb63c13c614872c7
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-08-27 18:36:53 +0700

    Buat file check_auth.go
    File tersebut berisi function untuk mengecek apakah bearer yang dikirimkan si client valid atau tidak.

---
+++ middlewares/check_auth.go
@@ -0,0 +1,31 @@
+package middlewares
+
+import (
+	"belajar-golang-jwt/helpers"
+	"net/http"
+	"strings"
+)
+
+var error = helpers.CustomError{}
+
+func CheckAuth(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		bearerToken := strings.Split(authHeader, " ")
+
+		if len(bearerToken) < 2 {
+			error.ApiError(w, http.StatusForbidden, "Token not provided!")
+			return
+		}
+
+		token := bearerToken[1]
+
+		_, err := helpers.VerifyJwtToken(token)
+		if err != nil {
+			error.ApiError(w, http.StatusForbidden, err.Error())
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}

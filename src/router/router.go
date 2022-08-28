e672e07a0c1b2b421145043c4509d4fa9edec659
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-08-28 10:32:35 +0700

    Update router
    Daftarkan API create dan show all food kedalam router. Dan API tersebut dicek menggunakan middleware check auth.

--- src/router/router.go
+++ src/router/router.go
@@ -2,6 +2,8 @@ package router
 
 import (
 	"belajar-golang-jwt/src/controllers"
+	"belajar-golang-jwt/src/helpers"
+	"belajar-golang-jwt/src/middlewares"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -10,10 +12,15 @@ import (
 
 func RegisterRoutes(db *gorm.DB) *mux.Router {
 	router := mux.NewRouter()
+	error := helpers.CustomError{}
 
-	userController := controllers.NewUserController(db)
+	userController := controllers.NewUserController(db, error)
 	router.HandleFunc("/auth/signup", userController.SignupUser()).Methods(http.MethodPost)
 	router.HandleFunc("/auth/login", userController.LoginUser()).Methods(http.MethodPost)
+
+	foodController := controllers.NewFoodController(db, error)
+	router.HandleFunc("/food", middlewares.CheckAuth(foodController.AddNewFoodItem())).Methods(http.MethodPost)
+	router.HandleFunc("/food", middlewares.CheckAuth(foodController.GetAllFoodItems())).Methods(http.MethodGet)
 
 	return router
 }
